bfad74227485e9dee0f533b9b2593b1b44c141d0
Abdullah Aydoğan <aydoganabdullah34@gmail.com>
2023-10-05 18:12:06 +0300

    buttons are coded

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,23 @@ const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
 const textarea = document.querySelector("textarea");
 
-const generateGradient = () => {
+const refreshBtn = document.querySelector(".refresh");
+const copyBtn = document.querySelector(".copy");
+
+const getRandomColor = () => {
+
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+
+    return `#${randomHex}`;
+}
+
+const generateGradient = (isRandom) => {
+
+    if(isRandom) {
+
+        colorInputs[0].value = getRandomColor();
+        colorInputs[1].value = getRandomColor();
+    }
 
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
 
@@ -11,9 +27,21 @@ const generateGradient = () => {
     textarea.value = `background: ${gradient};`;
 };
 
+const copyCode = () => {
+
+    navigator.clipboard.writeText(textarea.value);
+    copyBtn.innerText = "CSS Code Copied";
+
+    setTimeout(() => copyBtn.innerText = "Copy CSS Code", 1600);
+}
+
 colorInputs.forEach(input => {
 
-    input.addEventListener("input", generateGradient);
+    input.addEventListener("input", () => generateGradient(false));
 });
 
-selectMenu.addEventListener("change", generateGradient);
+selectMenu.addEventListener("change", () => generateGradient(false));
+
+refreshBtn.addEventListener("click", () => generateGradient(true));
+
+copyBtn.addEventListener("click", copyCode);
